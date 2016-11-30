22b76faf761d5aad65db1b4a36a88fe8d93ab2ca
VonAncken <mail@tobiasstampfl.de>
2016-11-30 21:04:14 +0100

    new information on download site

--- templates/de/download.inc.php
+++ templates/de/download.inc.php
@@ -2,28 +2,265 @@
     <div class="jumbotron">
       <div class="container">
 
-      <div class="row">
-        <div class="col-md-6">
-          <h1>
-            Downloads
-          </h1>
-          <p>
-            Find Matrix Games on GitHub
-          </p>
-        </div>
-        <div class="col-md-6 text-right">
-          <p>
-          <img src="images/python-white-64.png"/>
-          <img src="images/git-white-64.png"/>
-          </p>
-        </div>
-      </div>
-
+        <h1>
+          Downloads
+        </h1>
+        <p>
+          Find Matrix Games on GitHub
+        </p>
 
       </div>
     </div>
 
     <div class="container">
+      <h2>
+        What do you wanna do?
+      </h2>
+
+      <div class="panel-group" id="accordion">
+
+       <div class="panel panel-default">
+         <div class="panel-heading">
+           <div class="row" style="display: table;table-layout: fixed;">
+             <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+               <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                 <h4 class="panel-title">
+                   I just want to play the games / create games for myself
+                 </h4>
+               </a>
+             </div>
+             <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+               <table>
+                 <tr>
+                   <td> <img src="images/gamepad-black-32.png"/> </td>
+                 </tr>
+               </table>
+             </div>
+           </div>
+         </div>
+         <div id="collapse1" class="panel-collapse collapse">
+           <div class="panel-body">
+             This is the easiest way to start with Matrix Games because you can create or just play
+             Matrix Games without a devolopment environment. For this we offer you a package with
+             the windows binaries and the games, which are available as source code and are
+             dynamically loaded by a game console. This allows you to customize existing games or
+             create new ones. <br>
+             Download and unpack the package and you are good to go. <br>
+             <br>
+             <a href="download/MatrixGames.zip" class="btn btn-primary" role="button"> Download </a>
+           </div>
+         </div>
+       </div>
+
+       <div class="panel panel-default">
+         <div class="panel-heading">
+           <div class="row" style="display: table;table-layout: fixed;">
+             <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+               <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                 <h4 class="panel-title">
+                   I want create games and share them with the community
+                 </h4>
+               </a>
+             </div>
+             <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+               <table>
+                 <tr>
+                   <td> <img src="images/community-black-32.png"/> </td>
+                   <td>&nbsp;&nbsp;</td>
+                   <td> <img src="images/gamepad-black-32.png"/> </td>
+                 </tr>
+               </table>
+             </div>
+           </div>
+         </div>
+         <div id="collapse2" class="panel-collapse collapse">
+           <div class="panel-body">
+             So you want to support the community, great. Here is what you need:<br>
+             <br>
+             <div class="panel panel-info">
+               <div class="panel-heading">
+                 <div class="row" style="display: table;table-layout: fixed;">
+                   <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+                     First you have to set up a python development environment.
+                   </div>
+                   <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+                     <img src="images/python-black-24.png"/>
+                   </div>
+                 </div>
+               </div>
+               <div class="panel-body">
+                 <ol>
+                   <li>
+                     Download and install Python 3.4 <i>(recommended directory: `C:\Python34`):</i>
+                     <br> <br>
+                     <a href="https://www.python.org/ftp/python/3.4.4/python-3.4.4.msi"
+                        class="btn btn-primary" role="button"> Download </a>
+                   </li>
+                   <br>
+                   <li>
+                     Update PIP <i>(important)</i>
+                     <ul>
+                       <li>Open a console and change to your python installation folder.</li>
+                       <li>
+                         Execute following command:
+                         <pre>python -m pip install --upgrade pip</pre>
+                       </li>
+                     </ul>
+                   </li>
+                   <br>
+                   <li>
+                     Install the mtxPython Package
+                     <ul>
+                       <li>Open a console and change to the 'Scripts' directory of your python installation folder.</li>
+                       <li>
+                         Execute following command:
+                         <pre>pip install -U mtxPython</pre>
+                       </li>
+                     </ul>
+                   </li>
+                 </ol>
+               </div>
+             </div>
+             <div class="panel panel-info">
+               <div class="panel-heading">
+                 <div class="row" style="display: table;table-layout: fixed;">
+                   <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+                     Now become part of the community and get the MatrixGames repository.
+                   </div>
+                   <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+                     <img src="images/community-black-24.png"/>
+                   </div>
+                 </div>
+               </div>
+               <div class="panel-body">
+                 The Matrix Games project is available on <a href="https://github.com/MatrixGamesHub">GitHub</a>.<br>
+                 Fork and clone the <a href="https://github.com/MatrixGamesHub/MatrixGames"><b>MatrixGames repository</b></a>.
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+
+       <div class="panel panel-default">
+         <div class="panel-heading">
+           <div class="row" style="display: table;table-layout: fixed;">
+             <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+               <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                 <h4 class="panel-title">
+                   I would like to work on the framework (mtxPython, clients)
+                 </h4>
+               </a>
+             </div>
+             <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+               <table>
+                 <tr>
+                   <td> <img src="images/community-black-32.png"/> </td>
+                   <td>&nbsp;&nbsp;</td>
+                   <td> <img src="images/gamepad-black-32.png"/> </td>
+                 </tr>
+               </table>
+             </div>
+           </div>
+         </div>
+         <div id="collapse3" class="panel-collapse collapse">
+           <div class="panel-body">
+             Great that you want to support us and want to participate in the MatrixGames project. Here is what you need:<br>
+             <br>
+             <div class="panel panel-info">
+               <div class="panel-heading">
+                 <div class="row" style="display: table;table-layout: fixed;">
+                   <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+                     First you have to set up a python development environment.
+                   </div>
+                   <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+                     <img src="images/python-black-24.png"/>
+                   </div>
+                 </div>
+               </div>
+               <div class="panel-body">
+                 <ol>
+                   <li>
+                     Download and install Python 3.4 <i>(recommended directory: `C:\Python34`):</i>
+                     <br> <br>
+                     <a href="https://www.python.org/ftp/python/3.4.4/python-3.4.4.msi"
+                        class="btn btn-primary" role="button"> Download </a>
+                   </li>
+                   <br>
+                   <li>
+                     Update PIP <i>(important)</i>
+                     <ul>
+                       <li>Open a console and change to your python installation folder.</li>
+                       <li>
+                         Execute following command:
+                         <pre>python -m pip install --upgrade pip</pre>
+                       </li>
+                     </ul>
+                   </li>
+                   <br>
+                   <li>
+                     Install the mtxPython Package
+                     <ul>
+                       <li>Open a console and change to the 'Scripts' directory of your python installation folder.</li>
+                       <li>
+                         Execute following command:
+                         <pre>pip install -U mtxPython</pre>
+                       </li>
+                     </ul>
+                   </li>
+                 </ol>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+
+       <div class="panel panel-default">
+         <div class="panel-heading">
+           <div class="row" style="display: table;table-layout: fixed;">
+             <div class="col-md-12" style="display:table-cell;vertical-align:middle;float: none;">
+               <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+                 <h4 class="panel-title">
+                   I want it all
+                 </h4>
+               </a>
+             </div>
+             <div class="col-md-12 text-right" style="display:table-cell;vertical-align:middle;float: none;">
+               <table>
+                 <tr>
+                   <td> <img src="images/develop-black-32.png"/> </td>
+                   <td>&nbsp;&nbsp;</td>
+                   <td> <img src="images/community-black-32.png"/> </td>
+                   <td>&nbsp;&nbsp;</td>
+                   <td> <img src="images/gamepad-black-32.png"/> </td>
+                 </tr>
+               </table>
+             </div>
+           </div>
+         </div>
+         <div id="collapse4" class="panel-collapse collapse">
+           <div class="panel-body">
+           </div>
+         </div>
+       </div>
+
+      </div>
+
+
+
+
+
+<!--
+
+
+      <h2>
+        mtxPython
+      </h2>
+      <p>
+        The mtxPython package is hosted on <a href="https://pypi.org/project/mtxPython"> <b> PyPi
+        (Python Package Index) </b> </a> so you can easily install the package with pip:<br>
+        <pre>pip install -U mtxPython</pre>
+      </p>
+
       <h2>
         You want the whole project?
       </h2>
@@ -89,4 +326,7 @@ pip install -U sphinx_rtd_theme</pre>
           </ul>
         </li>
       </ul>
+
+-->
+
     </div> <!-- /container -->
