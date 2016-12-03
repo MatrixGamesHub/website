20dc45d7d017155a509e11811105ab2d4dc30941
VonAncken <mail@tobiasstampfl.de>
2016-12-03 01:14:20 +0100

    GameConsole-Page: screenshot added

--- templates/de/gameConsole.inc.php
+++ templates/de/gameConsole.inc.php
@@ -13,24 +13,33 @@
     </div>
 
     <div class="container">
-      <h2>
-        What are the tasks of the game console?
-      </h2>
-      <p>
-        The game console is responsible for several tasks.
-        <ul>
-          <li> managing the games </li>
-          <ul>
-            <li> list all available games </li>
-            <li> load a game </li>
-          </ul>
-          <li> providing a controller interface </li>
-          <li> sending changes on the playing field to the game renderers </li>
-          <li> managing the clients </li>
-          <li> optional: providing a game renderer </li>
+      <div class="row" style="display: table;table-layout: fixed;">
+        <div class="col-md-6">
+          <h2>
+            What are the tasks of the game console?
+          </h2>
+          <p>
+            The game console is responsible for several tasks.
+            <ul>
+              <li> managing the games </li>
+              <ul>
+                <li> list all available games </li>
+                <li> load a game </li>
+              </ul>
+              <li> providing a controller interface </li>
+              <li> sending changes on the playing field to the game renderers </li>
+              <li> managing the clients </li>
+              <li> optional: providing a game renderer </li>
 
-        </ul>
-      </p>
+            </ul>
+          </p>
+        </div>
+        <div class="col-md-6">
+          <img src="images/gameConsole/screenshot1.png"/>
+        </div>
+      </div>
+
+
 
       <h2>
         Controller Interface
